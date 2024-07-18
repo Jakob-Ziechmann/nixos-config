4cf32796d2617e2460b1f255112042a36ef19e6b
Jakob Ziechmann <abc@xyz>
2024-07-18 22:59:01 +0200

    renamed home-manager directory

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
+        modules = [./home/home.nix];
       };
     };
   };
