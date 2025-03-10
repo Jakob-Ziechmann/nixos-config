5a00fc8ab015467fdb9ceaf5eefce6ba322deb9a
= <>
2025-03-10 23:51:18 +0100

    changed bash to be bash

--- home/shell/zsh/default.nix
+++ home/shell/zsh/default.nix
@@ -1,33 +1,33 @@
-{ config, lib, ... }: {
-  imports = [
-    ./p10k.nix
-    ./fzf.nix
-    ./completion.nix
-    ./history.nix
-    ./syntaxHL.nix
-  ];
+{ lib, ... }: {
+	imports = [
+		./p10k.nix
+		./fzf.nix
+		./completion.nix
+		./history.nix
+		./syntaxHL.nix
+	];
 
-  zsh.enableP10k = lib.mkDefault true;
-  zsh.p10kInstantPrompt = lib.mkDefault true;
-  zsh.fzf-tab = lib.mkDefault true;
-  zsh.completions = lib.mkDefault true;
-  zsh.leanHistory = lib.mkDefault true;
-  zsh.syntaxHighlighting = lib.mkDefault true;
+	zsh.enableP10k = lib.mkDefault true;
+	zsh.p10kInstantPrompt = lib.mkDefault true;
+	zsh.fzf-tab = lib.mkDefault true;
+	zsh.completions = lib.mkDefault true;
+	zsh.leanHistory = lib.mkDefault true;
+	zsh.syntaxHighlighting = lib.mkDefault true;
 
-  programs.zsh = {
-    enable = true;
-    antidote.enable = true;
+	programs.zsh = {
+		enable = true;
+		antidote.enable = true;
 
-    defaultKeymap = "viins";
+		defaultKeymap = "viins";
 
 		sessionVariables = {
 			EDITOR = "nvim";
 			VISUAL = "nvim";
 		};
-  };
+	};
 
-  programs.zoxide = {
-    enable = true;
-    options = ["--cmd cd"];
-  };
+	programs.zoxide = {
+		enable = true;
+		options = ["--cmd cd"];
+	};
 }
--- modules/shell.nix
+++ modules/shell.nix
@@ -1,8 +1,11 @@
-{ ... }: {
-	system.activationScripts.binbash = {
-		deps = [ "binsh" ];
-		text = ''
-			ln -sf /bin/sh /bin/bash
-		'';
-	};
+{ pkgs, ... }:
+{
+	environment.systemPackages = with pkgs; [
+		bash
+	];
+
+	environment.variables.BASH = "${pkgs.bash}/bin/bash";
+	environment.shells = with pkgs; [ bash ];
+	environment.pathsToLink = [ "/bin" ];
+	environment.etc."bin/bash".source = "${pkgs.bash}/bin/bash";
 }
